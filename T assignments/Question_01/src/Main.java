98369d3018f660424c5a341532952ccb3360f515
Utsav Kumar Choudhary <choudharyutsav2@gmail.com>
2025-08-13 16:41:07 +0530

    --

--- T assignments/Question_01/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Manager manager = new Manager("Alice", 101, 80000.0, 5);
-        Developer developer = new Developer("Bob", 102, 60000.0, "Java");
-
-        manager.displayDetails();
-        developer.displayDetails();
-    }
-}
